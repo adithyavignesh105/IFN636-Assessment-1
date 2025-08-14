f3c6b12ff0a89ae5b623b6630f06651c6d5f4a71
Hannah Law <hannah.lch.law@gmail.com>
2025-08-15 02:14:01 +1000

    Basic Config update

--- frontend/src/axiosConfig.jsx
+++ frontend/src/axiosConfig.jsx
@@ -2,7 +2,7 @@ import axios from 'axios';
 
 const axiosInstance = axios.create({
   baseURL: 'http://localhost:5001', // local
-  //baseURL: 'http://13.236.186.95:5001', // live
+  //baseURL: 'http://13.55.94.57:5001', // live
   headers: { 'Content-Type': 'application/json' },
 });
 
